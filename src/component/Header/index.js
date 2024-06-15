365512334b163faf8930e694c755c0c42cbf4c31
tnssganesh <165705047+tnssganesh@users.noreply.github.com>
2024-06-15 21:48:49 +0530

    Update index.js

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -14,25 +14,14 @@ const Header = props => {
   return (
     <nav className="nav-header">
       <div className="nav-content">
-        <div className="nav-bar-mobile-logo-container">
+        <div className="nav-bar-large-container">
           <Link to="/">
             <img
               className="website-logo"
-              src="https://assets.ccbp.in/frontend/react-js/logo-img.png "
+              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
               alt="website logo"
             />
           </Link>
-          <button type="button" className="nav-mobile-btn">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/logo-img.png "
-              alt="website logo"
-              className="nav-bar-image"
-              onClick={onClickLogout}
-            />
-          </button>
-        </div>
-
-        <div className="nav-bar-large-container">
           <ul className="nav-menu">
             <li className="nav-menu-item">
               <Link to="/" className="nav-link">
@@ -68,19 +57,10 @@ const Header = props => {
           </li>
 
           <li className="nav-menu-item-mobile">
-            <Link to="/products" className="nav-link">
+            <Link to="/jobs" className="nav-link">
               <img
                 src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
                 alt="nav products"
-                className="nav-bar-image"
-              />
-            </Link>
-          </li>
-          <li className="nav-menu-item-mobile">
-            <Link to="/cart" className="nav-link">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
-                alt="nav cart"
                 className="nav-bar-image"
               />
             </Link>
